507ac3418f9a87ec737454699f8a529343b27275
Mateus <fabriciomateus05@gmail.com>
2019-08-30 15:32:06 -0300

    Commit antes de iniciar o desenvolvimento

---
+++ SourceCode/OrganizadorGeometrico/Controller/DXF.cs
@@ -0,0 +1,23 @@
+﻿using netDxf;
+using netDxf.Header;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizadorGeometrico.Controller
+{
+    class DXF
+    {
+        public void Teste()
+        {
+            bool version;
+            DxfDocument aquivoBase = DxfDocument.Load(@"C:\Users\ROBOSOFT_MATEUS\Desktop\Estagio Supervisionado\Estagio 1\Arquivo Morto\DXF\Peça1.DXF");
+            DxfVersion a = DxfDocument.CheckDxfFileVersion("", out version);
+
+            //Entender a estrutura do arquivo DXF
+
+        }
+    }
+}
--- SourceCode/OrganizadorGeometrico/View/Form_Principal.cs
+++ SourceCode/OrganizadorGeometrico/View/Form_Principal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrganizadorGeometrico.Controller;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,19 @@ namespace OrganizadorGeometrico
 {
     public partial class Form_Principal : Form
     {
+        private DXF dxf;
+
         public Form_Principal()
         {
+            //dxf = new DXF();
+            //dxf.Teste();
             InitializeComponent();
         }
 
         private void BtnImportarPlanoGeometrico_Click(object sender, EventArgs e)
         {
-
+            //FileDialog fileDialog;
+            //fileDialog.ShowDialog();
         }
 
         private void BtnRemoverPlanoGeometrico_Click(object sender, EventArgs e)
